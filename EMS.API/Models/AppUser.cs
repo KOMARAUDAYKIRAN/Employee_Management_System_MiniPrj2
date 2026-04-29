d7be53e0e27c7b205f9693ac7f7613490c6bc5c8
Uday Kiran <komaraudaykiran2003@gmail.com>
2026-04-29 19:37:33 +0530

    Initial commit

---
+++ EMS.API/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EMS.API.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
